f0381307e3fe1fbfeee0948aa7396901802d199a
shivamFullStack111 <shvam12340987@gmail.com>
2025-08-19 10:12:02 +0530

    Update index.js

--- multiple-docker-continer-run-in-single-pod-of-kubernetes/backend/index.js
+++ multiple-docker-continer-run-in-single-pod-of-kubernetes/backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
+//mongodb:27017 in below mongodb is name of kubernetes mongo service we can access mongo by their k8s deployment service name because both backend and mongo are deployed in same pod
 mongoose.connect(`mongodb://mongodb:27017/my-app`).then(()=>{
 	console.log("db connected")
 }).catch(err=>console.log(err.message))
